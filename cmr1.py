c11b88a3d5a2e8bdccdda7a7cbdb536df18a996c
mahavvrms <31892940+mahavvrms@users.noreply.github.com>
2017-09-14 10:46:30 +0530

    Add files via upload

---
+++ cmr1.py
@@ -0,0 +1,4 @@
+#user/bin/python3
+import random
+r=random.randiant(1,6)
+print(r)
---
+++ dice.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+r=random.randint(1,6)
+print(r)
+if r==6:
+	print ("congraulation")
+	
